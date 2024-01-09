8fe37d9ce5034fe044ea529f43a0e18be73474d0
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-09 12:03:00 -0500

    Finished Dog Structure

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,18 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Dog {
+    
+    // Stored properties
+    let breed: String
+    let color: String
+    let eyecolor: String
+    var gender: String
+    var heightInMetres: Double
+    var lengthInMetres: Double
+    var massInKg: Double
+    
+    // Computed properties
+        var heightInCentimetres: Double {
+            return heightInMetres * 100
+        }
+}
