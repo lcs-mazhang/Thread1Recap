dc3b48195b3b9020ab63d3a43fb528989fb0f6c2
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-11 14:20:08 -0500

    Creating color and making first gradient

--- Home/Home/ContentView.swift
+++ Home/Home/ContentView.swift
@@ -7,17 +7,33 @@
 
 import SwiftUI
 
+// Stored property
+let purpleToRed = Gradient(colors: [Color.purple, Color.red])
+
+// Computed property
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100)
-            Text("Hello, world!")
-                .font(.largeTitle)
+        ZStack {
+            
+            // bottom layer
+            LinearGradient(
+                gradient: purpleToRed,
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            // top layer
+            VStack {
+                Image(systemName: "globe")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                Text("Hello, world!")
+                    .font(.largeTitle)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
