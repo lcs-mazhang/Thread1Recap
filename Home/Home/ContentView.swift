70fbd43dd7aeba8b119664a41e5a230e5aeddcad
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-11 14:12:01 -0500

    Made the first step of the globe

---
+++ Home/Home/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Home
+//
+//  Created by GengYu Zhang on 2024-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100)
+            Text("Hello, world!")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
