109d37f555647c97d8d6682899666c20208482d3
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-13 15:31:41 -0500

    Finished 2 one from left to right

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -5,8 +5,8 @@ struct Dog {
     // Stored properties
     let breed: String
     let color: String
-    let eyecolor: String
-    var gender: String
+    let eyeColor: String
+    var spotColor: String
     var heightInMetres: Double
     var lengthInMetres: Double
     var massInKg: Double
@@ -21,12 +21,22 @@ struct Dog {
         }
 }
 
-var dogAtMostLeft = Dog(
+var dog1FromLeftToRight = Dog(
     breed: "Bulldog",
     color: "White with brown pattern",
-    eyecolor: "Black",
-    gender: "unknown",
+    eyeColor: "Black",
+    spotColor: "Brown",
     heightInMetres: 0.5,
+    lengthInMetres: 0.6,
+    massInKg: 1.2
+)
+
+var dog2FromLeftToRight = Dog(
+    breed: "Bernese Mountain Dog",
+    color: "Black",
+    eyeColor: "Black",
+    spotColor: "Brown",
+    heightInMetres: 0.6,
     lengthInMetres: 0.7,
-    massInKg: 1.5
+    massInKg: 1.4
 )
